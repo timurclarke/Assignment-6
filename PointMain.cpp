649fc8200cebf4d0243e155b14bdbd020858cd23
timurclarke <timur.clarke@gmail.com>
2016-07-15 21:29:40 -0400

    More minor comments

--- PointMain.cpp
+++ PointMain.cpp
@@ -11,11 +11,12 @@ using namespace std;
 
 int main()
 {
-	Point p1(-1.5, 0);
+	Point p1(-17.0, 1.5);
+	Point p2(-17.0, 10);
 
-	Point p2(1.5, 4.0);
+	double dist = p1.distanceTo(p2);
 
-	cout << "Distance p1 to p2: " << p1.distanceTo(p2) << endl;
+	cout << "Distance p1 to p2: " << dist << endl;
 
 	//metro code below
 
@@ -24,9 +25,13 @@ int main()
 	//cout << "LS.getEnd1" << LS.getEnd1() << endl;
 	//cout << "LS.getEnd1" << LS.getEnd2() << endl;
 
-	cout << "LS length: " << LS.length() << endl;
+	double length = LS.length();
 
-	cout << LS.slope() << endl;
+	cout << "LS length: " << length << endl;
+
+	double slope = LS.slope();
+
+	cout << "LS slope: " << slope << endl;
 
 	return 0;
 }
